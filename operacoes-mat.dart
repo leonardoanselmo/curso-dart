d59a0a248abb90647bca92fe0f4994b6793d61ad
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2020-02-20 20:37:50 -0300

    Exemplo de operações matemáticas

---
+++ operacoes-mat.dart
@@ -0,0 +1,15 @@
+main(){
+
+  var adicao = 2 + 3;
+  var subtracao = 5 - 2;
+  var multiplicacao = 2*4;
+  var divisao = 10/2;
+  var resto = 10 % 2;
+  
+  print(adicao);
+  print(subtracao);
+  print(multiplicacao);
+  print(divisao);
+  print(resto);
+
+}
