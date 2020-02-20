4b4988a680632a64a40fe74710de588f865c2373
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2020-02-20 20:54:32 -0300

    Exemplo de input

---
+++ maior-idade.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+main() {
+
+  print("Informe uma idade: ");
+  var input = stdin.readLineSync();
+
+  var idade = int.parse(input);
+
+  if (idade >= 18) {
+    print("maior de idade");    
+  } else {
+    print("menor de idade");
+  }
+
+}
