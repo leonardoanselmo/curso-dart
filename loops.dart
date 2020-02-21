0daa706dfde21552ce33a8076efed17fc0786178
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2020-02-21 09:52:40 -0300

    Exemplos de loops

---
+++ loops.dart
@@ -0,0 +1,13 @@
+main(){
+
+  for (var i = 0; i < 10; i++) {
+    print("Leonardo $i");  
+  }  
+
+  int valor = 0;  
+  while (valor <= 10) {
+    print("O valor é: $valor");
+    valor++;
+  }
+
+}
